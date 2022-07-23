43cd203b70b1e9ce9db3511fb8906af1e8dd33e5
RzkGnz <92670982+RzkGnz@users.noreply.github.com>
2022-07-23 14:51:56 +0700

    Add files via upload

--- rzki.js
+++ rzki.js
@@ -2632,7 +2632,24 @@ case prefix+'topbalance':
 case prefix+'leaderboard':
 addCountCmd('#topbalance', m.sender, _cmd)
 try {
-require('./lib/lb.js')(args, rzki, from, m)
+//require('./lib/lb.js')(args, rzki, from, m)
+//let name = m.fromMe ? zansky.user : zansky.contacts[m.sender] 
+let sortedLimit = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
+//let sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
+let sortedmoney = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].Money - a[1].Money)
+let usersLimit = sortedLimit.map(v => v[0])
+//let usersLim = sortedLim.map(v => v[0])
+let usersmoney = sortedmoney.map(v => v[0])
+let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(20, sortedLimit.length)
+let rpgt = `❏  *T O P - B A L A N C E*
+
+${sortedmoney.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + '\n   Saldo :' + data.Money + ' ').join`\n`}
+`.trim()
+rzki.sendText(from, rpgt, m, {
+contextInfo: {
+mentions: parseMention(rpgt)
+}
+})
 } catch (err) {
 console.log(err)
 }
